78392d356a7cdf4563a9e3abe6e78d19562e71bd
David_Launay <david@debian-BULLSEYE-live-builder-AMD64>
2023-07-20 19:36:15 +0200

    Second commit by React

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/styles.scss";
+import App from "./components/App";
+import reportWebVitals from "./reportWebVitals";
+import i18next from "i18next";
+import { initReactI18next, useTranslation } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from "i18next-http-backend";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+i18next
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    detection: {
+      order: ["localStorage", "cookie", "htmlTag", "path", "subdomain"],
+      caches: ["cookie"],
+    },
+    debug: true,
+    lng: document.querySelector("html").lang,
+    fallbackLng: "fr",
+    backend: {
+      loadPath: "/assets/locales/{{lng}}/translation.json",
+    },
+  });
+
+export default i18next;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
